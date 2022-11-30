b412ae6d71937e0e8e88fbfb1499c74784ddfbe2
Francisco <kikopocampo@gmail.com>
2022-11-29 22:26:25 -0700

    install and use axios

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,28 +1,9 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 
 import "components/Application.scss";
 import DayList from "./DayList";
-import Appointment from "components/Appointment/index"
-
-
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+import Appointment from "components/Appointment/index";
+import axios from "axios";
 
 const appointments = {
   "1": {
@@ -64,8 +45,15 @@ const appointments = {
 };
 
 export default function Application(props) {
-
+  
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:8001/api/days")
+      .then(result => setDays(result.data))
+      .catch(err => console.log(err.message))
+  }, [])
 
   const appointmentArr = Object.values(appointments).map(appointment => {
     return (<Appointment 
--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,14 +2,14 @@ import React from "react";
 import DayListItem from "./DayListItem";
 
 export default function DayList(props){
-  const days = props.days.map (days => {
+  const days = props.days.map (day => {
     return(
       <DayListItem
-        key={days.id}
-        name={days.name}
-        spots={days.spots}
-        selected={days.name === props.value}
-        setDay={() => props.onChange(days.name)}
+        key={day.id}
+        name={day.name}
+        spots={day.spots}
+        selected={day.name === props.value}
+        setDay={() => props.onChange(day.name)}
       />
     )
   })
